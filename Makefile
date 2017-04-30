67de0a93f0f579a52362efe7b6c99b1e36a91632
Ahmed Fasih <wuzzyview@gmail.com>
2017-04-30 00:18:59 -0400

    Preliminary quiz API developing

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+all: client/client.js
+
 client/client.js: client.js
-	node_modules/.bin/browserify client.js -s client -o client/client.js
+	node_modules/.bin/browserify -t brfs client.js -s client -o client/client.js
+
+watch:
+	fswatch -0 -o -l .1 client.js | xargs -0 -n 1 -I {} make
--- client.js
+++ client.js
@@ -2,6 +2,54 @@
 var choo = require('choo');
 var html = require('choo/html');
 var app = choo();
-var main = function() { return html`<div>choo choo</div>` };
+app.use((state, emitter) => {
+  state.name = "Sally";
+  state.quiz = null;
+
+  function wrapRender(f) {
+    return data => {
+      f(data);
+      emitter.emit('render')
+    };
+  }
+  emitter.on('nameChange', wrapRender(data => state.name = data));
+  emitter.on('pickQuiz', wrapRender(data => state.quiz = data));
+  emitter.on('clearQuiz', wrapRender(data => state.quiz = null));
+});
+function main(state, emit) {
+  console.log(state);
+  var quizHtml;
+  if (state.quiz) {
+    quizHtml = administerQuiz(state.quiz, x => console.log('result,', x));
+  }
+  return html`<div>
+  <button onclick=${hitClick}>Hit me ${state.name}</button>
+  ${quizHtml}
+  </div>`;
+
+  function hitClick(e) { emit('pickQuiz', pickQuiz()); }
+}
 app.route('/', main);
 app.mount('#app');
+
+///////////////// Quizzer!
+
+var fs = require('fs');
+var tono = JSON.parse(fs.readFileSync('data/tono.json', 'utf8'));
+
+// Administer a quiz
+function administerQuiz(picked, resultCallback) {
+  var [num, field] = picked;
+  return html`<div>I picked ${num} and ${field}!
+  </div>`;
+}
+
+// Pick a fact (and any specifics, like sub-fact) to quiz
+function pickQuiz() {
+  const topics = [ 'kanjis', 'readings', 'meanings' ];
+  const quiz = [ randi(5000), topics[randi(3)] ];
+  console.log('picked', quiz);
+  return quiz;
+}
+
+function randi(n) { return Math.floor(Math.random() * n); }
